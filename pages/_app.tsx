a2d51ddd28981cad3b99e60f7e037ae4b513cc2c
EduardoDomingues <eeadomingues@gmail.com>
2023-05-05 20:41:52 -0300

    edit profile functionality

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,11 +5,13 @@ import { Toaster } from 'react-hot-toast';
 import { SessionProvider } from 'next-auth/react';
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
+import EditModal from '@/components/modals/EditModal';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <SessionProvider session={pageProps.session}>
       <Toaster />
+      <EditModal />
       <RegisterModal />
       <LoginModal />
       <Layout>
