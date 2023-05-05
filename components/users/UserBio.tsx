17e30ca02102e4df9a853f0cd2a7b2e738c4fc69
EduardoDomingues <eeadomingues@gmail.com>
2023-05-05 16:09:39 -0300

    user profile

--- components/users/UserBio.tsx
+++ components/users/UserBio.tsx
@@ -1,5 +1,6 @@
 import { format } from 'date-fns';
 import { useMemo } from 'react';
+import { BiCalendar } from 'react-icons/bi';
 
 import useCurrentUser from "@/hooks/useCurrentUser";
 import useUser from "@/hooks/useUser";
@@ -31,7 +32,54 @@ const UserBio: React.FC<UserBioProps> = ({ userId }) => {
                     <Button onClick={() => {}} label='Follow' secondary />
                 )}
             </div>
-
+            <div className='mt-8 px-4'>
+                <div className='flex flex-col'>
+                    <p className='text-white text-2xl font-semibold'>
+                        {fetchedUser?.name}
+                    </p>
+                    <p className='text-md text-neutral-500'>
+                        @{fetchedUser?.username}
+                    </p>
+                </div>
+                <div className='flex flex-col mt-4'>
+                    <p className='text-white'>
+                        {fetchedUser?.bio}
+                    </p>
+                    <div 
+                    className='
+                            flex
+                            flex-row
+                            items-center
+                            gap-2
+                            mt-4
+                            text-neutral-500
+                        '
+                    >
+                        <BiCalendar size={24} />
+                        <p>
+                            Joined {createdAt}
+                        </p>
+                    </div>
+                </div>
+                <div className='flex flex-row items-center mt-4 gap-6'>
+                    <div className='flex flex-row items-center gap-1'>
+                        <p className='text-white'>
+                            {fetchedUser?.followingIds?.length}
+                        </p>
+                        <p className='text-neutral-500'>
+                            Following
+                        </p>
+                    </div>
+                    <div className='flex flex-row items-center gap-1'>
+                        <p className='text-white'>
+                            {fetchedUser?.followersCount || 0}
+                        </p>
+                        <p className='text-neutral-500'>
+                            Followers
+                        </p>
+                    </div>
+                </div>
+            </div>
         </div>
      );
 }
