8fe2199e07fa85eee707426a6a58a92e75843224
Eduardo Domingues <eeadomingues@gmail.com>
2023-05-12 15:34:31 -0300

    Welcome to Tuliter

--- components/Form.tsx
+++ components/Form.tsx
@@ -103,7 +103,7 @@ const Form: React.FC<FormProps> = ({
             ) : (
             <div className="py-8">
                 <h1 className="text-white text-2xl text-center mb-4 font-bold">
-                   Welcome to Twitter 
+                   Welcome to Tuliter
                 </h1>
                 <div className="flex flex-row items-center justify-center gap-4">
                     <Button label='Login' onClick={loginModal.onOpen}/>
